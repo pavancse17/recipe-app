425144c374ffbe4f8e1e842dbb91c36005dba43a
Pavan Kumar <pavancse17@gmail.com>
2019-07-21 09:06:06 +0530

    Add recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe {
+  public name: string;
+  public descrption: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imsgePath: string) {
+    this.name = name;
+    this.descrption = desc;
+    this.imagePath = this.imagePath;
+  }
+}
